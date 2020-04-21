450fe60ecdd420d65e98116ffc305421a3d927a8
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-04-21 10:00:27 +0200

    implement translations

--- src/JobsBundle/Resources/public/js/plugin.js
+++ src/JobsBundle/Resources/public/js/plugin.js
@@ -8,6 +8,18 @@ pimcore.plugin.Jobs = Class.create(pimcore.plugin.admin, {
 
     initialize: function () {
         pimcore.plugin.broker.registerPlugin(this);
+
+        if (!String.prototype.format) {
+            String.prototype.format = function () {
+                var args = arguments;
+                return this.replace(/{(\d+)}/g, function (match, number) {
+                    return typeof args[number] != 'undefined'
+                        ? args[number]
+                        : match
+                        ;
+                });
+            };
+        }
     },
 
     uninstall: function () {
