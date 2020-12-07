004556a8298fa733d43c1b5665455bce4fdb6f5d
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-12-07 17:50:56 +0100

    [Tests] move to github actions

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,7 +3,7 @@
 use Pimcore\Bootstrap;
 use DachcomBundle\Test\Util\Autoloader;
 
-include __DIR__ . '/../../../vendor/autoload.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 define('PIMCORE_KERNEL_CLASS', '\DachcomBundle\Test\App\TestAppKernel');
 define('PIMCORE_TEST', true);
@@ -11,7 +11,11 @@ define('PIMCORE_TEST', true);
 Bootstrap::setProjectRoot();
 Bootstrap::bootstrap();
 
+/**
+ * @var $loader \Composer\Autoload\ClassLoader
+ */
 Autoloader::addNamespace('Pimcore\Tests', PIMCORE_PROJECT_ROOT . '/vendor/pimcore/pimcore/tests/_support');
+Autoloader::addNamespace('Pimcore\Model\DataObject', __DIR__ . '/_output/var/classes/DataObject');
 
 if (!defined('TESTS_PATH')) {
     define('TESTS_PATH', __DIR__);
@@ -24,3 +28,4 @@ if (!isset($_SERVER['REQUEST_URI'])) {
 if (!isset($_SERVER['HTTP_USER_AGENT'])) {
     $_SERVER['HTTP_USER_AGENT'] = '';
 }
+
