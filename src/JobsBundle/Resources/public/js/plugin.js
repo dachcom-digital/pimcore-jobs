008b2c2555450ca92744a0704da903d5cf956589
pascalmoser <pascalmoser@gmail.com>
2023-02-08 17:12:09 +0100

    pimcore 10.5 ready

--- src/JobsBundle/Resources/public/js/plugin.js
+++ src/JobsBundle/Resources/public/js/plugin.js
@@ -1,59 +1,27 @@
-pimcore.registerNS('pimcore.plugin.Jobs');
+document.addEventListener(pimcore.events.pimcoreReady, (e) => {
 
-pimcore.plugin.Jobs = Class.create(pimcore.plugin.admin, {
+    var user = pimcore.globalmanager.get('user'),
+        openSettings = function (config) {
+            try {
+                pimcore.globalmanager.get('jobs_bundle_settings').activate();
+            } catch (e) {
+                pimcore.globalmanager.add('jobs_bundle_settings', new Jobs.SettingsPanel(config));
+            }
+        };
 
-    getClassName: function () {
-        return 'pimcore.plugin.Jobs';
-    },
+    if (!user.isAllowed('jobs_permission_settings')) {
+        return false;
+    }
 
-    initialize: function () {
-        pimcore.plugin.broker.registerPlugin(this);
+    jobsMenu = new Ext.Action({
+        id: 'jobs_bundle_setting_button',
+        text: t('jobs.settings.configuration'),
+        iconCls: 'jobs_icon_bundle',
+        handler: openSettings.bind(this)
+    });
 
-        if (!String.prototype.format) {
-            String.prototype.format = function () {
-                var args = arguments;
-                return this.replace(/{(\d+)}/g, function (match, number) {
-                    return typeof args[number] != 'undefined'
-                        ? args[number]
-                        : match
-                        ;
-                });
-            };
-        }
-    },
-
-    uninstall: function () {
-        // void
-    },
-
-    pimcoreReady: function (params, broker) {
-
-        var jobsMenu, user = pimcore.globalmanager.get('user');
-
-        if (!user.isAllowed('jobs_permission_settings')) {
-            return false;
-        }
-
-        jobsMenu = new Ext.Action({
-            id: 'jobs_bundle_setting_button',
-            text: t('jobs.settings.configuration'),
-            iconCls: 'jobs_icon_bundle',
-            handler: this.openSettingsPanel.bind(this)
-        });
-
-        if (layoutToolbar.settingsMenu) {
-            layoutToolbar.settingsMenu.add(jobsMenu);
-        }
-    },
-
-    openSettingsPanel: function () {
-        try {
-            pimcore.globalmanager.get('jobs_bundle_settings').activate();
-        } catch (e) {
-            pimcore.globalmanager.add('jobs_bundle_settings', new Jobs.SettingsPanel());
-        }
+    if (layoutToolbar.settingsMenu) {
+        layoutToolbar.settingsMenu.add(jobsMenu);
     }
 
 });
-
-new pimcore.plugin.Jobs();
