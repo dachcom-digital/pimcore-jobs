29078eab7079e0919f2e5b5e818c42ba1b32ffe5
Stefan Hagspiel <info@solverat.com>
2023-08-30 16:03:30 +0200

    support i18n link generator, adjust readme

--- src/Service/LinkGeneratorService.php
+++ src/Service/LinkGeneratorService.php
@@ -2,12 +2,20 @@
 
 namespace JobsBundle\Service;
 
+use I18nBundle\Builder\RouteParameterBuilder;
+use I18nBundle\LinkGenerator\I18nLinkGeneratorInterface;
 use JobsBundle\Model\ConnectorContextItemInterface;
 use Pimcore\Model\DataObject\ClassDefinition\LinkGeneratorInterface;
 use Pimcore\Model\DataObject\Concrete;
+use Pimcore\Model\Site;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LinkGeneratorService implements LinkGeneratorServiceInterface
 {
+    public function __construct(protected UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
     public function generate(Concrete $object, ConnectorContextItemInterface $contextItem): ?string
     {
         $linkGeneratorContext = [
@@ -17,6 +25,28 @@ class LinkGeneratorService implements LinkGeneratorServiceInterface
 
         $dataUrl = null;
         $linkGenerator = $object->getClass()->getLinkGenerator();
+
+        // support for i18n
+        if ($linkGenerator instanceof I18nLinkGeneratorInterface) {
+
+            $definition = $contextItem->getContextDefinition();
+
+            $context = [];
+            $routeParameter = [
+                '_locale' => $definition->getLocale(),
+            ];
+
+            $pimcoreSite = Site::getByDomain(parse_url($definition->getHost(), PHP_URL_HOST));
+
+            if ($pimcoreSite instanceof Site) {
+                $context['site'] = $pimcoreSite;
+            }
+
+            $routeItemParameters = RouteParameterBuilder::buildForEntity($object, $routeParameter, $context);
+
+            return $this->urlGenerator->generate('', $routeItemParameters, UrlGeneratorInterface::ABSOLUTE_URL);
+        }
+
         if ($linkGenerator instanceof LinkGeneratorInterface) {
             $dataUrl = $linkGenerator->generate($object, $linkGeneratorContext);
         }
